ca69c3f95554b3013f8f5114c5c2149a5102731f
linwz1102628136 <landseven7@163.com>
2022-06-21 02:02:00 +0800

    登录模块开发完成

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,20 @@
 import { Button, NavBar, Form, Input, Toast } from "antd-mobile"
-import styles from "./index.module.scss"
 import { LoginForm } from "@/types/data"
 import { login, getCode } from "@/store/action/login"
 import { useDispatch } from "react-redux"
 import { useHistory } from "react-router-dom"
-import type { AxiosError } from "axios"
-import { useRef } from "react"
+import { useRef, useState, useEffect } from "react"
 import { InputRef } from "antd-mobile/es/components/input"
+import type { AxiosError } from "axios"
+import styles from "./index.module.scss"
 const Login = () => {
   const dispatch = useDispatch()
   const history = useHistory()
   // 得到一个 Form 表单实例
   const [form] = Form.useForm()
   const mobileRef = useRef<InputRef>(null)
+  const timerRef = useRef(-1)
+  const [timeleft, setTimeleft] = useState(0)
   // 提交表单
   const handleSubmit = async (value: LoginForm) => {
     try {
@@ -32,6 +34,20 @@ const Login = () => {
       })
     }
   }
+  // 监听 timeleft
+  useEffect(() => {
+    if (timeleft === 0) { 
+      // 时间结束后，清除定时器
+      window.clearInterval(timerRef.current)
+    }
+  }, [timeleft])
+  // useEffect 可以写多个
+  useEffect(() => {
+    // return 的这个函数会在组件卸载时执行
+    return () => {
+      clearInterval(timerRef.current)
+    }
+  },[])
   // 发送验证码
   const sendCode = async () => {
     const mobile = (form.getFieldValue("mobile") || "") as string
@@ -48,6 +64,19 @@ const Login = () => {
         content: "验证码已发送",
         duration: 1000,
       })
+
+      // 开始计时
+      setTimeleft(5)
+      // 开始倒计时
+      /* 注意：     --只要useState 状态发生了变化，函数就会重新执行，此时useState 中的状态是最新的，
+                  所以需要一个能够记住定时器标识的东西 ---> useRef, ref对象上的current 属性表示
+                  当前 dom 实例，它不会在函数更新过程中发生改变 
+                  -- 此处需要使用 window.setInterval
+                  因为 setInterval 默认返回 NodeJS.Timeout，使用 window.setInterval 后，返回值才是 number 类型的数值
+                  */
+      timerRef.current = window.setInterval(() => {
+        setTimeleft((timeleft) => timeleft - 1)
+      }, 1000)
     } catch (error) {
       // 这里需要将 error 对象 断言为 Axios的错误对象
       const e = error as AxiosError<{ message: string }>
@@ -87,8 +116,13 @@ const Login = () => {
           <Form.Item
             className="login-item"
             extra={
-              <span className="code-extra" onClick={sendCode}>
-                发送验证码
+              <span
+                className="code-extra"
+                // 判断是否开启定时器，没开启绑定事件，开启后去掉事件
+                onClick={timeleft === 0 ? sendCode : undefined}
+              >
+                {/*判断是否开启定时器，没开启绑定事件，开启后去掉事件*/}
+                {timeleft === 0 ? "发送验证码" : `${timeleft}秒后可发送`}
               </span>
             }
             name="code"
